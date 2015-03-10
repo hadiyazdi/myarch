34e066afab802a6e618f615244d13267bf26309e
Hadi Yazdi <hadiawahab@gmail.com>
2015-03-10 23:37:30 +0300

    Add NetworkManager applet and necessary tools  Changes to be committed: 	new file:   ../etc/skel/.nanorc 	modified:   customize_airootfs.sh 	modified:   ../../packages.both
    Add .nanorc settings "disable word wrap in nano"
    
    NetworkManager:
     - Add network-manager-applet
     - Add gnome-icon-theme
     - Add gnome-keyring
    
     - enable and start NetworkManager.service

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -21,5 +21,7 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
+systemctl start NetworkManager.service
+systemctl enable NetworkManager.service
 
 pacman -Syu
